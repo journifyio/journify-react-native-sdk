92e31dbd0d454e061e933f482713f69b4b636c81
Iliyass Hamza <iliyass.hamza@journify.io>
2024-09-02 13:43:48 +0100

    fix: add sementic slack

--- release.config.js
+++ release.config.js
@@ -12,10 +12,13 @@ module.exports = {
     '@semantic-release/changelog',
     'semantic-release-yarn',
     ["@semantic-release/github", {
-      "prerelease": true
+      "prerelease": true,
     }],
     '@semantic-release/git',
-    
+    ["@intuit/semantic-release-slack", {
+        // These are the available platforms that the package can be downloaded from
+        "platforms": ["npm"]
+    }]
   ],
   debug: true
 };
