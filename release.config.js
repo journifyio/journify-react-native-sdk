9f15db85c8502d0be2a22de5653ab82721fa524e
Iliyass Hamza <iliyass.hamza@gmail.com>
2024-11-19 12:45:08 +0100

    feat: remove slack

--- release.config.js
+++ release.config.js
@@ -18,9 +18,5 @@ module.exports = {
       },
     ],
     "@semantic-release/git",
-    // ["@intuit/semantic-release-slack", {
-    //     // These are the available platforms that the package can be downloaded from
-    //     "platforms": ["npm"]
-    // }]
   ],
 };
