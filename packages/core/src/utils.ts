944009eb26d183e4595363602b16b9a93731d93a
Iliyass Hamza <iliyass.hamza@gmail.com>
2024-11-19 11:37:03 +0100

    add utils and export JournifyProvider (#8)
    * add utils and export JournifyProvider
    
    * rollback chunk

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -191,3 +191,114 @@ const sizeOf = (obj: unknown): number => {
   const size = encodeURI(JSON.stringify(obj)).split(/%..|./).length - 1;
   return size / 1024;
 };
+
+export function isNumber(x: unknown): x is number {
+  return typeof x === 'number';
+}
+
+export function isString(x: unknown): x is string {
+  return typeof x === 'string';
+}
+
+export function isBoolean(x: unknown): x is boolean {
+  return typeof x === 'boolean';
+}
+
+export function isDate(value: unknown): value is Date {
+  return (
+    value instanceof Date ||
+    (typeof value === 'object' &&
+      Object.prototype.toString.call(value) === '[object Date]')
+  );
+}
+
+export function objectToString(value: object, json = true): string | undefined {
+  // If the object has a custom toString we well use that
+  if (value.toString !== Object.prototype.toString) {
+    return value.toString();
+  }
+  if (json) {
+    return JSON.stringify(value);
+  }
+  return undefined;
+}
+
+export function unknownToString(
+  value: unknown,
+  stringifyJSON = true,
+  replaceNull: string | undefined = '',
+  replaceUndefined: string | undefined = ''
+): string | undefined {
+  if (value === null) {
+    if (replaceNull !== undefined) {
+      return replaceNull;
+    } else {
+      return undefined;
+    }
+  }
+
+  if (value === undefined) {
+    if (replaceUndefined !== undefined) {
+      return replaceUndefined;
+    } else {
+      return undefined;
+    }
+  }
+
+  if (isNumber(value) || isBoolean(value) || isString(value)) {
+    return value.toString();
+  }
+
+  if (isObject(value)) {
+    return objectToString(value, stringifyJSON);
+  }
+
+  if (stringifyJSON) {
+    return JSON.stringify(value);
+  }
+  return undefined;
+}
+
+/**
+ * Checks if value is a dictionary like object
+ * @param value unknown object
+ * @returns typeguard, value is dicitonary
+ */
+export const isObject = (value: unknown): value is Record<string, unknown> =>
+  value !== null &&
+  value !== undefined &&
+  typeof value === 'object' &&
+  !Array.isArray(value);
+
+/**
+ * Utility to deeply compare 2 objects
+ * @param a unknown object
+ * @param b unknown object
+ * @returns true if both objects have the same keys and values
+ */
+export function deepCompare<T>(a: T, b: T): boolean {
+  // Shallow compare first, just in case
+  if (a === b) {
+    return true;
+  }
+
+  // If not objects then compare values directly
+  if (!isObject(a) || !isObject(b)) {
+    return a === b;
+  }
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (const key of keysA) {
+    if (!keysB.includes(key) || !deepCompare(a[key], b[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
