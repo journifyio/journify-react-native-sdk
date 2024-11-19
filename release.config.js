a50a7070b10c15eccbd10967ecce56026fd3c976
Iliyass Hamza <iliyass.hamza@gmail.com>
2024-11-19 12:39:23 +0100

    feat: disable now slack

--- release.config.js
+++ release.config.js
@@ -1,23 +1,26 @@
 module.exports = {
   ci: false,
-  branches: ["main", {
-    name: "beta",
-    prerelease: true
-  }],
+  branches: [
+    "main",
+    {
+      name: "beta",
+      prerelease: true,
+    },
+  ],
   plugins: [
+    ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+    "@semantic-release/changelog",
+    "semantic-release-yarn",
     [
-      '@semantic-release/commit-analyzer', 
-      { preset: 'conventionalcommits' }
+      "@semantic-release/github",
+      {
+        prerelease: true,
+      },
     ],
-    '@semantic-release/changelog',
-    'semantic-release-yarn',
-    ["@semantic-release/github", {
-      "prerelease": true,
-    }],
-    '@semantic-release/git',
-    ["@intuit/semantic-release-slack", {
-        // These are the available platforms that the package can be downloaded from
-        "platforms": ["npm"]
-    }]
-  ]
+    "@semantic-release/git",
+    // ["@intuit/semantic-release-slack", {
+    //     // These are the available platforms that the package can be downloaded from
+    //     "platforms": ["npm"]
+    // }]
+  ],
 };
