590e8a42c0425bb220ee804125b8ac55a10becc7
Iliyass Hamza <iliyass.hamza@gmail.com>
2025-01-21 10:33:33 +0100

    chore: add husky validate commit msg

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,60 @@
 module.exports = {
   extends: [
     "@react-native",
-    'plugin:@typescript-eslint/recommended',
-    "prettier"
+    "plugin:@typescript-eslint/recommended",
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   rules: {
-    "no-void": [
-      "error", 
-      { "allowAsStatement": true }
-    ],
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/restrict-template-expressions": ["warn", { 
-      allowNumber: true,
-      allowBoolean: true,
-      allowAny: true,
-      allowNullish: true
-     }],
-     "@typescript-eslint/no-explicit-any": "error",
-     "@typescript-eslint/strict-boolean-expressions": "error",
     "prettier/prettier": [
-      "error",
+      "warn",
       {
-        "quoteProps": "consistent",
-        "singleQuote": true,
-        "tabWidth": 2,
-        "trailingComma": "es5",
-        "useTabs": false
-      }
-    ]
+        quoteProps: "consistent",
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: "es5",
+        useTabs: false,
+      },
+    ],
+    "no-void": ["error", { allowAsStatement: true }],
+    "@typescript-eslint/restrict-template-expressions": [
+      "warn",
+      {
+        allowNumber: true,
+        allowBoolean: true,
+        allowAny: true,
+        allowNullish: true,
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/strict-boolean-expressions": "error",
   },
+  overrides: [
+    // Detox tests
+    {
+      files: ["*.e2e.js"],
+      env: {
+        jest: true,
+      },
+    },
+    // Jest
+    {
+      files: ["**/__tests__/**", "**/__mocks__/**", "**/__helpers__/**"],
+      plugins: ["jest"],
+      env: {
+        jest: true,
+      },
+      rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unsafe-call": "warn",
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/unbound-method": "off",
+        "@typescript-eslint/no-unsafe-assignment": "warn",
+        "@typescript-eslint/no-unsafe-member-access": "warn",
+        "@typescript-eslint/ban-ts-comment": "warn",
+      },
+    },
+  ],
   parserOptions: {
-    project: ['./tsconfig.linter.json'],
+    project: ["./tsconfig.linter.json"],
   },
 };
--- release.config.js
+++ release.config.js
@@ -1,22 +1,10 @@
 module.exports = {
-  ci: false,
-  branches: [
-    "main",
-    {
-      name: "beta",
-      prerelease: true,
-    },
-  ],
   plugins: [
     ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
     "@semantic-release/changelog",
     "semantic-release-yarn",
-    [
-      "@semantic-release/github",
-      {
-        prerelease: true,
-      },
-    ],
+    "@semantic-release/github",
     "@semantic-release/git",
   ],
+  debug: true,
 };
