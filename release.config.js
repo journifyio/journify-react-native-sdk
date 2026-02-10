bf27d35ef057343fe938b1eb8ab4bbb387b09911
Iliyass Hamza <iliyass.hamza@gmail.com>
2026-02-10 14:34:42 +0100

    ci: enable Slack notifications in publish workflow and release configuration

--- release.config.js
+++ release.config.js
@@ -18,5 +18,9 @@ module.exports = {
       },
     ],
     "@semantic-release/git",
+    ["@intuit/semantic-release-slack", {
+        // These are the available platforms that the package can be downloaded from
+        "platforms": ["npm"]
+    }]
   ],
 };
